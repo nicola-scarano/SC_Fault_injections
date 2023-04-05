cad8097ead696223217ca41f9fdb3b73fcd11b7d
Juan David Guerrero Balaguera <jguerrero@legionlogin.local>
2023-04-05 23:01:06 +0200

    Setupt fro conda environmet

--- install_environment.sh
+++ install_environment.sh
@@ -2,8 +2,11 @@
 
 # git pull --recurse-submodules 
 
-cp Pipfile ../Pipfile
+cp environment.yaml ../environment.yaml
 
 cd ..
 
-pipenv install
+conda env create -f environment.yaml
+conda activate sc2-benchmark
+pip install -e ./torchdistill-0.3.3/
+pip install -e ./SC_Fault_injections/pytorchfi_SC/

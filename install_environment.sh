d8a97a5f1c23a92193fe1a918f7006c7879e6062
divadnauj-GB <juan.guerrero@polito.it>
2023-04-06 01:16:58 +0200

    update installer

--- install_environment.sh
+++ install_environment.sh
@@ -8,5 +8,7 @@ cd ..
 
 conda env create -f environment.yaml
 conda activate sc2-benchmark
-pip install -e ./torchdistill-0.3.3/
+
+pip install -e .
+#pip install -e ./torchdistill-0.3.3/
 pip install -e ./SC_Fault_injections/pytorchfi_SC/

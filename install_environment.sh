76ed59560538f73257d77549ed153ea9d2d815a0
divadnauj-GB <juan.guerrero@polito.it>
2023-04-06 01:34:20 +0200

    fix again the conda install

--- install_environment.sh
+++ install_environment.sh
@@ -7,7 +7,7 @@ cp environment.yaml ../environment.yaml
 cd ..
 
 conda env create -f environment.yaml
-source /home/jguerrero/miniconda3/etc/profile.d/conda.sh
+source ~/miniconda3/etc/profile.d/conda.sh
 
 conda activate sc2-benchmark
 

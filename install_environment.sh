74abe9e63c8128779bedd4d1f87b77056fabdcdd
divadnauj-GB <juan.guerrero@polito.it>
2023-04-07 21:46:12 +0000

    update conda instalation script

--- install_environment.sh
+++ install_environment.sh
@@ -7,10 +7,12 @@ cp environment.yaml ../environment.yaml
 cd ..
 
 conda env create -f environment.yaml
-source ~/miniconda3/etc/profile.d/conda.sh
-
+source ~/miniconda3/bin/activate
+conda init bash
 conda activate sc2-benchmark
 
-pip install -e .
+python -m pip install -e .
 #pip install -e ./torchdistill-0.3.3/
-pip install -e ./SC_Fault_injections/pytorchfi_SC/
+python -m pip install -e ./SC_Fault_injections/pytorchfi_SC/
+
+python -m pip list

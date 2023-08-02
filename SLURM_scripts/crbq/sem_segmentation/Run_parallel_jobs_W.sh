5af100e663b63108a6851ef0fecce3281a63e42f
giuseppeesposito98 <giuseppeesposito98@libero.it>
2023-08-02 13:23:03 +0200

    fix: bad baddissimo

---
+++ SLURM_scripts/crbq/sem_segmentation/Run_parallel_jobs_W.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PWD=`pwd`
+
+global_PWD="$PWD"
+
+DIR="$1"
+target_layer="$2"
+
+
+mkdir -p ${global_PWD}/${DIR}
+
+input_args=(1 3 2 6 9 12 77)
+
+array_size=${#input_args[@]}
+
+for ((i=0; i<$array_size; i++)); do
+    sbatch --output=$DIR/cnf${input_args[$((i))]}_lyr${target_layer}_stdo_%A_%a.log --error=$DIR/cnf${input_args[$((i))]}_lyr${target_layer}_stde_%A_%a.log ${global_PWD}/SC_Fault_injections/SLURM_scripts/crbq/sem_segmentation/Weight_cfg_FI.sbatch ${input_args[$((i))]} $target_layer ${DIR}
+done
